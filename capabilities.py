9ece06daa675b602ba070f165687fc092e950019
Andrey Stoletov <stoletov314@gmail.com>
2024-03-13 22:37:46 +0100

    Capabilities was added,a few steps for starting

--- capabilities.py
+++ capabilities.py
@@ -1,1 +1,1 @@
-medical_charges = "https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv"
+medical_charges_url = "https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv"
