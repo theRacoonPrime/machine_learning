5c6e53ad31657b54bd384b8805625400f9482926
Andrey Stoletov <stoletov314@gmail.com>
2024-03-17 03:11:34 +0100

    First steps in machine learning

--- learning/machine_learning.py
+++ learning/machine_learning.py
@@ -6,13 +6,48 @@ import scipy.stats as stats
 import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import plotly.express as px
 
 file_path = '/Users/andrey/Downloads/data_csv.csv'
-data = pd.read_csv(file_path)
+df = pd.read_csv(file_path)
+# print(df)
+# data_show = pd.DataFrame({
+#     'Location': ['AUS', 'LTU', 'RUS'],
+#     'Time': [1971, 1972, 1975],
+#     'PC_Health': [15.992, 11.849, 28.942],
+# })
 
-data_show = pd.DataFrame({
-    'Location': ['AUS', 'LTU', 'RUS'],
-    'Time': [1971, 1972, 1975],
-    'PC_Health': [15.992, 11.849, 28.942],
-})
-print(data.info())
+# df.info()
+# plt.figure(figsize=(5, 5))
+# sns.pairplot(df)
+# plt.show()
+#
+# fig = px.line(df, x='TIME', y='TOTAL_SPEND', color='LOCATION')
+# fig.show()
+
+# grcprt = df[df['LOCATION'].isin(['PRT', 'GRC'])]
+# plt.figure(figsize=(14,6))
+# sns.lineplot(data=grcprt, x='TIME', y='TOTAL_SPEND', hue='LOCATION')
+# plt.show()
+# plt.figure(figsize=(14,6))
+# sns.set_style('darkgrid')
+# lineplot = sns.lineplot(data=df, x='TIME', y='TOTAL_SPEND', hue='LOCATION')
+# num_items_per_row = 10
+# num_legend_rows = -(-len(df['LOCATION'].unique()) // num_items_per_row)
+# plt.legend(title='LOCATION', bbox_to_anchor=(0.5, -0.1), loc='upper center', ncol=num_items_per_row)
+# plt.tight_layout(rect=[0, 0.1, 1, 1])
+# plt.show()
+# plt.figure(figsize=(12,8))
+
+sns.lmplot(data=df, x='USD_CAP', y='PC_GDP',
+           height=8, aspect=1.5,
+           scatter_kws={'s': 50, 'alpha': 0.5},
+           line_kws={'lw': 2})
+plt.xlabel('USD per Capita')
+plt.ylabel('% of GDP')
+plt.title('Linear Regression of USD_CAP vs PC_GDP')
+
+plt.tight_layout()
+plt.show()
+
+
