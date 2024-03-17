05672caf81c22d75e43123e1577a1336e749f42c
Andrey Stoletov <stoletov314@gmail.com>
2024-03-18 00:19:00 +0100

    Code refractoring and visualisation was added

--- learning/machine_learning.py
+++ learning/machine_learning.py
@@ -8,7 +8,10 @@ from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.model_selection import (
     train_test_split, GridSearchCV,cross_val_score)
 import plotly.express as px
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import (mean_squared_error,
+                             mean_absolute_error,
+                             r2_score)
+
 
 file_path = '/Users/andrey/Downloads/data_csv.csv'
 df = pd.read_csv(file_path)
@@ -62,6 +65,7 @@ df = pd.read_csv(file_path)
 # plot_lmplot(df, x='USD_CAP', y='PC_GDP')
 
 
+
 X = df[['USD_CAP']]
 y = df['PC_GDP']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -75,7 +79,7 @@ param_grid = {'alpha': [0.1, 1, 10]}  # Adjust the range of alpha values
 
 grid_search = GridSearchCV(ridge, param_grid, cv=5, scoring='neg_mean_squared_error')
 
-# Fit the grid search to the training data
+
 grid_search.fit(X_train, y_train)
 
 
@@ -85,7 +89,22 @@ print("Best Alpha:", best_alpha)
 
 best_model = grid_search.best_estimator_
 y_pred = best_model.predict(X_test)
+print("Best Model:", best_model)
 
 
 mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
 print("Mean Squared Error:", mse)
+print("Mean Absolute Error:", mae)
+print("R^2 Score:", r2)
+
+
+plt.scatter(X_test, y_test, color='blue', label='Actual')
+plt.plot(X_test, y_pred, color='red', linewidth=2, label='Predicted')
+plt.xlabel('USD per Capita')
+plt.ylabel('% of GDP')
+plt.title('Ridge Regression')
+plt.legend()
+plt.show()
