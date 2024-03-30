92643931573a7cdfd7a2f5000d45e6122fe6cbf2
Andrey Stoletov <stoletov314@gmail.com>
2024-03-30 21:52:53 +0100

    first analysis

--- learning/Heart_Attack_Dataset.py
+++ learning/Heart_Attack_Dataset.py
@@ -5,20 +5,25 @@ import seaborn as sns
 df = pd.read_csv('/Users/andrey/Downloads/Medicaldataset.csv')
 
 
+# plt.figure(figsize=(10, 6))
+# sns.histplot(df['Age'], bins=20, kde=True, color='skyblue')
+# plt.title('Age Distribution')
+# plt.xlabel('Age')
+# plt.ylabel('Frequency')
+# plt.show()
+#
+#
+# plt.figure(figsize=(6, 6))
+# sns.countplot(x='Gender', data=df, palette='pastel')
+# plt.title('Gender Distribution')
+# plt.xlabel('Gender')
+# plt.ylabel('Count')
+# plt.show()
+
+
 plt.figure(figsize=(10, 6))
-sns.histplot(df['Age'], bins=20, kde=True, color='skyblue')
-plt.title('Age Distribution')
-plt.xlabel('Age')
+sns.histplot(df['Heart rate'], bins=20, kde=True, color='salmon')
+plt.title('Heart Rate Distribution')
+plt.xlabel('Heart Rate')
 plt.ylabel('Frequency')
 plt.show()
-
-
-plt.figure(figsize=(6, 6))
-sns.countplot(x='Gender', data=df, palette='pastel')
-plt.title('Gender Distribution')
-plt.xlabel('Gender')
-plt.ylabel('Count')
-plt.show()
-
-
-
