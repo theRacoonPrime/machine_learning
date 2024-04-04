43c8f08ff0bcd30b0901c5479901b1782852e628
Andrey Stoletov <stoletov314@gmail.com>
2024-04-04 04:16:04 +0200

    Basic setup for CNN

--- learning/Brain_Tumor_Detection_with_CNN_Model.py
+++ learning/Brain_Tumor_Detection_with_CNN_Model.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import shutil
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from keras.callbacks import EarlyStopping
 from keras.regularizers import l2
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+
+path = '/kaggle/input/brain-mri-scan-images-tumor-detection/brain_mri_scan_images'
+data_path = path.split(os.sep)
