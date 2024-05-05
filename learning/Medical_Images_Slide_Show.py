acbcb3226d4d06b47ca047cace86274a98cdbe4a
Andrey Stoletov <stoletov314@gmail.com>
2024-05-05 04:30:42 +0200

    Comments were added for better understanding

--- learning/Medical_Images_Slide_Show.py
+++ learning/Medical_Images_Slide_Show.py
@@ -10,9 +10,13 @@ import dicom
 import warnings
 warnings.filterwarnings("ignore")
 
+# Path to data
 pathway = "/Users/andrey/Downloads/archive"
+
+# Read overview data
 data_df = pd.read_csv(os.path.join(pathway, "overview.csv"))
 
+# Function to process image data
 def process_data(path):
     data = pd.DataFrame([{'Path': filepath} for filepath in glob(pathway + path)])
     data['File'] = data['Path'].map(os.path.basename)
@@ -22,9 +26,13 @@ def process_data(path):
     data['Modality'] = data['File'].map(lambda x: str(x.split('_')[6].split('.')[-2]))
     return data
 
+# Process TIFF image data
 tiff_data = process_data('/tiff_images/*.tif')
+
+# Process DICOM image data
 dicom_data = process_data('/dicom_dir/*.dcm')
 
+# Function to compare countplots of a feature across different datasets
 def countplot_comparison(feature):
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16, 4))
     s1 = sns.countplot(data_df[feature], ax=ax1)
@@ -38,14 +46,19 @@ def countplot_comparison(feature):
 
     plt.show()
 
+# Function to read image
 def readImg(img):
     # Load the input image
     image = imread(img)
     return image
 
+# Read DICOM images
 dicomImg = [readImg(path) for path in dicom_data["Path"]]
+
+# Read TIFF images
 tiffImg = [readImg(path) for path in tiff_data["Path"]]
 
+# Image resizing
 image_size = 256
 dim = (image_size, image_size)
 
@@ -53,16 +66,25 @@ def resize(img):
     resized = cv2.resize(img, dim, interpolation=cv2.INTER_CUBIC)
     return resized
 
+# Resize DICOM images
 resizedImg = [resize(img) for img in dicomImg]
+
+# Resize TIFF images
 resizedImg_tiff = [resize(img) for img in tiffImg]
 
+# Image normalization
 def normalize(img):
     normalized = cv2.normalize(img, None, alpha=0, beta=200, norm_type=cv2.NORM_MINMAX)
     return normalized
 
+# Normalize DICOM images
 normalizedImg = [normalize(img) for img in resizedImg]
+
+# Normalize TIFF images
 normalizedImg_tiff = [normalize(img) for img in resizedImg_tiff]
 
+# Reshape and normalize image data
 shapedData = [img.reshape(-1)/ 255.0 for img in normalizedImg]
 shapedData_tiff = [img.reshape(-1)/ 255.0 for img in normalizedImg_tiff]
+
 
