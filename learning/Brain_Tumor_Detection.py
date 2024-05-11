714a5304d5a08aec9840eb84162bf9bf0931e683
Andrey Stoletov <stoletov314@gmail.com>
2024-05-11 04:42:54 +0200

    setup brain_tumor detection , rewriting defs , importing

--- learning/Brain_Tumor_Detection.py
+++ learning/Brain_Tumor_Detection.py
@@ -5,47 +5,16 @@ import matplotlib.pyplot as plt
 import cv2
 from tqdm.notebook import tqdm  # Importing tqdm for progress bars
 import tensorflow as tf
+from list_of_defs import  parse_txt_annot
 
 BATCH_SIZE = 4  # Batch size for training
 GLOBAL_CLIPNORM = 10.0  # Global clipnorm value for gradient clipping
 
 AUTO = tf.data.AUTOTUNE  # Autotune parameter for performance optimization
 
-# Function to parse text annotations
-def parse_txt_annot(img_path, txt_path):
-    img = cv2.imread(img_path)
-    w = int(img.shape[0])
-    h = int(img.shape[1])
-
-    file_label = open(txt_path, "r")
-    lines = file_label.read().split('\n')
-
-    boxes = []
-    classes = []
-
-    if lines[0] == '':
-        return img_path, classes, boxes
-    else:
-        for i in range(0, int(len(lines))):
-            objbud = lines[i].split(' ')
-            class_ = int(objbud[0])
-
-            x1 = float(objbud[1])
-            y1 = float(objbud[2])
-            w1 = float(objbud[3])
-            h1 = float(objbud[4])
-
-            xmin = int((x1 * w) - (w1 * w) / 2.0)
-            ymin = int((y1 * h) - (h1 * h) / 2.0)
-            xmax = int((x1 * w) + (w1 * w) / 2.0)
-            ymax = int((y1 * h) + (h1 * h) / 2.0)
-
-            boxes.append([xmin, ymin, xmax, ymax])
-            classes.append(class_)
-
-    return img_path, classes, boxes
-
 # Function to create a list of file paths
+
+
 def create_paths_list(path):
     full_path = []
     images = sorted(os.listdir(path))
@@ -56,6 +25,7 @@ def create_paths_list(path):
     return full_path
 
 # Mapping class ids to class labels
+
 class_ids = ['label0', 'label1', 'label2']
 class_mapping = dict(zip(range(len(class_ids)), class_ids))
 
