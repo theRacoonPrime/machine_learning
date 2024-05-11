a7a607be0841b83d9b4434cdb71e384547d47efc
Andrey Stoletov <stoletov314@gmail.com>
2024-05-11 04:49:48 +0200

    minor refractoring Medical_Images file , were added a few def to list of defs

--- learning/Medical_Images_Slide_Show.py
+++ learning/Medical_Images_Slide_Show.py
@@ -8,6 +8,7 @@ import cv2
 from skimage.io import imread
 import dicom
 import warnings
+from list_of_defs import (countplot_comparison, normalize)
 warnings.filterwarnings("ignore")
 
 # Path to data
@@ -17,6 +18,7 @@ pathway = "/Users/andrey/Downloads/archive"
 data_df = pd.read_csv(os.path.join(pathway, "overview.csv"))
 
 # Function to process image data
+
 def process_data(path):
     data = pd.DataFrame([{'Path': filepath} for filepath in glob(pathway + path)])
     data['File'] = data['Path'].map(os.path.basename)
@@ -27,24 +29,11 @@ def process_data(path):
     return data
 
 # Process TIFF image data
+
 tiff_data = process_data('/tiff_images/*.tif')
 
 # Process DICOM image data
 dicom_data = process_data('/dicom_dir/*.dcm')
-
-# Function to compare countplots of a feature across different datasets
-def countplot_comparison(feature):
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16, 4))
-    s1 = sns.countplot(data_df[feature], ax=ax1)
-    s1.set_title("Overview Data")
-
-    s2 = sns.countplot(tiff_data[feature], ax=ax2)
-    s2.set_title("Tiff Images")
-
-    s3 = sns.countplot(dicom_data[feature], ax=ax3)
-    s3.set_title("Dicom Images")
-
-    plt.show()
 
 # Function to read image
 def readImg(img):
@@ -53,6 +42,7 @@ def readImg(img):
     return image
 
 # Read DICOM images
+
 dicomImg = [readImg(path) for path in dicom_data["Path"]]
 
 # Read TIFF images
@@ -72,12 +62,8 @@ resizedImg = [resize(img) for img in dicomImg]
 # Resize TIFF images
 resizedImg_tiff = [resize(img) for img in tiffImg]
 
-# Image normalization
-def normalize(img):
-    normalized = cv2.normalize(img, None, alpha=0, beta=200, norm_type=cv2.NORM_MINMAX)
-    return normalized
-
 # Normalize DICOM images
+
 normalizedImg = [normalize(img) for img in resizedImg]
 
 # Normalize TIFF images
--- learning/list_of_defs.py
+++ learning/list_of_defs.py
@@ -1,3 +1,6 @@
+import cv2
+
+
 def parse_txt_annot(img_path, txt_path):
     img = cv2.imread(img_path)
     w = int(img.shape[0])
@@ -30,3 +33,22 @@ def parse_txt_annot(img_path, txt_path):
             classes.append(class_)
 
     return img_path, classes, boxes
+
+
+def countplot_comparison(feature):
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16, 4))
+    s1 = sns.countplot(data_df[feature], ax=ax1)
+    s1.set_title("Overview Data")
+
+    s2 = sns.countplot(tiff_data[feature], ax=ax2)
+    s2.set_title("Tiff Images")
+
+    s3 = sns.countplot(dicom_data[feature], ax=ax3)
+    s3.set_title("Dicom Images")
+
+    plt.show()
+
+
+def normalize(img):
+    normalized = cv2.normalize(img, None, alpha=0, beta=200, norm_type=cv2.NORM_MINMAX)
+    return normalized
