1d3e60831f63a45088be7df15ebadffc53822539
Andrey Stoletov <stoletov314@gmail.com>
2024-05-11 04:37:15 +0200

    capabilities file was delleted, list of defs was added

---
+++ learning/list_of_defs.py
@@ -0,0 +1,32 @@
+def parse_txt_annot(img_path, txt_path):
+    img = cv2.imread(img_path)
+    w = int(img.shape[0])
+    h = int(img.shape[1])
+
+    file_label = open(txt_path, "r")
+    lines = file_label.read().split('\n')
+
+    boxes = []
+    classes = []
+
+    if lines[0] == '':
+        return img_path, classes, boxes
+    else:
+        for i in range(0, int(len(lines))):
+            objbud = lines[i].split(' ')
+            class_ = int(objbud[0])
+
+            x1 = float(objbud[1])
+            y1 = float(objbud[2])
+            w1 = float(objbud[3])
+            h1 = float(objbud[4])
+
+            xmin = int((x1 * w) - (w1 * w) / 2.0)
+            ymin = int((y1 * h) - (h1 * h) / 2.0)
+            xmax = int((x1 * w) + (w1 * w) / 2.0)
+            ymax = int((y1 * h) + (h1 * h) / 2.0)
+
+            boxes.append([xmin, ymin, xmax, ymax])
+            classes.append(class_)
+
+    return img_path, classes, boxes
